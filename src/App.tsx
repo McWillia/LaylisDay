b718c3365115ac7a71d285f0a8725cd9c5c0271c
Steven McWilliam <steve-mcw@live.co.uk>
2019-08-08 23:23:22 +0100

    Changed to typescript. Included react strap.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'reactstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        I have text <Button color='primary' > BNuttttton </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
